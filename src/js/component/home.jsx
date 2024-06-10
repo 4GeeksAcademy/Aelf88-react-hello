577d47479733cc51ca1f577be440c95e6046291c
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-06-10 04:15:42 +0000

    boton de borrar, añadir y funcion handleclick funcionales

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [newTodo, setNewestTodo] = useState("New todo example");
+    const [todos, setTodos] = useState(["First test task", "Second test task"]);
+    const handleClick = () => {
+        console.log("New todo example:", newTodo);
+        setTodos([...todos, newTodo]);
+        setNewestTodo("");
+    }
+    const deleteTask = (index) => {
+        const newList = todos.filter((todo,i) => i !== index)
+        console.log(index)
+        setTodos(newList)
+    }
+    const handleChange = (event) => {
+        setNewestTodo(event.target.value);
+    }
+    return (
+        <div className="text-center">
+            <h1 className="text-center mt-5">To-do List with Fetch!</h1>
+            <input type="text" value={newTodo} onChange={handleChange} />
+            <button onClick={handleClick}>
+                Add task
+            </button>
+            <p>New task: {newTodo}</p>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo} <button onClick={() => deleteTask(index)} >Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
-
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
